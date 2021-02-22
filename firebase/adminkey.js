9f779aa229a71f488d0d348fab6636fb88514fd6
LogeshSelvaraj <logeshksr99@gmail.com>
2021-02-22 12:02:22 +0530

    firebase error fixed

---
+++ firebase/adminkey.js
@@ -0,0 +1,16 @@
+const serviceAccount={
+    "type": "service_account",
+  "project_id": "ecommerce-6a1b0",
+  "private_key_id": "db90947f94d1294c4bb3860fab4a54befca5d6af",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDIfLTcXDYPTjZ7\n8kuYd2nY9g8WQXOb1QON/XxBOz6xfjXFhkalnFLVRr+VotZLTTiGkR+jZnGsf/gd\noTr0+cLQ6xseqK71iGmgAhqBSF81WLqVWVA22khow2s2c+ySEionMnpW9furctE9\nfGILG5bAHwDkXDRu240/ZQwTJ+CS1q+Zb2Rb+r9PSiaUfBtcRAlavLZJJX5X1Pru\n/wJy04o6tE9FqKpi3WEY2HkUiHvYKo9QVis1iqPaBqe+pO95Iaimu17Zk/sRU5FD\nlmnq2gnuRl/Rl2aCSwonv+dGH/j5hrg/Ea9ISk9kcsVhb5YAzLv3vee1mm+6cml0\n98lPLsdjAgMBAAECggEAFIuTDTH16EOfmbwOuyUhl7Y/fYxfiIyfvKFJZnXnytCg\nOgm0m6wVYP3LDE6Pn0/ehwvALkKFS6QhjQfEvu4JZNdzBAVNSKQeDuOoS69a8mCR\nXlS0v6pG2VWNWxsuFxn9b97gYqD6lWtCvgZNFc5R+IY01TINvX6KTwDP5/aiMOnY\ntoTwVKOOJme6JGwRQr6ISwbAu+8JqWEK7oLcY6d14PcLP+IBw4AEqCSi8a9NCaAO\nvPResRMKDAaSBmfWQVUGg+jo3dbSa/aV7epjIjrW+62fSwUcARPfX5YWsyoEYqrh\n4SoBswoupgiksngRcBjdRaw7P6EZ5kC7u/6iFOp94QKBgQDxSFg3b2mfK61x2JMn\nyG+J6Hy30jLQKpauO8gTJKIMtIyqLTKB33WKV0bYjDiJz/9VOK3i+MzRtOw3gz+e\npfz8wToFfb8aENvXsqVsyp5Un2iv4M34cRdRp1Mh8S2x7m713qa3BwsdrvbEFBAZ\n+d8A0cGjXGmse16aHIr/tibDPQKBgQDUt1YJpJrHBVnnC0N0nSODzv04dDZRJzGh\n8uEil7C+6w5TkGitkEw8S8+3xaw9UVBDgc/JLFxsyXG5/bnpjo7zN7WeBMg2vLtZ\npHIXoelTyFzjTI85E35qJt5GbXeOEa1QLG3o98KPeIXPyGNzyYwY0l+kt0CPvE8L\nsksDGMffHwKBgAY+eQn2OCWGW9a2ZBiDd196RL/9EJOShpO7uFHGgOsTQRCmWshB\noCkUWSEBf/URDOqHrPhSt/o1VGebXeodIY7o8TdzKZ7jEM1kMiO+mppWBb+Hcc0c\nIB5c1inqs+QG55g1Stsxs3fxLzGj00bY0IyU4Q35NkLgbnJOispTTG2BAoGBAKwR\nBYrVWNJG1NtdNBqfhfv4MsM41js9HhvnkQ/fbnxxPWyBMvu67sATfxojz1jmbqu8\n1Ozhcn4ZUWJnCL0RsTayyLnjGmWUs2Qm7peJRGmeeOzwoCZGS4romAKWSBBm4jAp\nUFsUF5DgqDVU3oXp8eaePfnKtnHOnI5UkfMWV493AoGBAMaCp0UmOjmq+cIWem/p\nM0PwEF2oqqGEEN7Gidio/uT1lLaRoB9W7bjKx2ONl+xAsZgrcuj/YJgMSj8AiMzc\nQfXYqqv0JMEUxbDQghAtTiQVFI4yEC61g950iNDybD50Wui1LjOAFrHiULrD58i5\nrwTuabrGpZBabBlSmypCiFKv\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-azudi@ecommerce-6a1b0.iam.gserviceaccount.com",
+  "client_id": "108514623863226425897",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-azudi%40ecommerce-6a1b0.iam.gserviceaccount.com"
+}
+
+module.exports={
+    serviceAccount
+}
--- firebase/index.js
+++ firebase/index.js
@@ -1,6 +1,7 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("../fb-admin-key.json");
+// var serviceAccount = require("../fb-admin-key.json");
+var {serviceAccount} = require("./adminkey");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
