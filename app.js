cc9fdd57f85b75374f689243aee05125e37075ab
LogeshSelvaraj <logeshksr99@gmail.com>
2021-02-22 09:29:46 +0530

    deploy

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const fs =require("fs")
+const path = require("path")
 
 const app=express()
 
@@ -21,16 +22,22 @@ mongoose
     console.log(err);
   });
 
-// middlewares
+// middleware
 app.use(morgan("dev"))
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json({limit:"2mb"}))
 app.use(cors())
-
+app.use(express.static(`client/build`))
 
 
 // importing all routes
 fs.readdirSync("./routes").map((r)=> app.use("/api",require("./routes/"+r)))
+
+
+app.get('*',(req,res)=>{
+  res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+})
+
 
 
 app.listen(process.env.PORT || 8000,()=>{
